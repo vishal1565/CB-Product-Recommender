82acd279a05404270851b9bfc2f5fe664e96a762
Keyur Shah <84345139+Kur1310@users.noreply.github.com>
2023-07-14 14:51:20 +0530

    Update jupyter.py

--- notebook/conf/jupyter.py
+++ notebook/conf/jupyter.py
@@ -13,10 +13,10 @@ c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
 
 # ipython -c "from notebook.auth import passwd; passwd()"
-#c.NotebookApp.password = u'sha1:8da45965a489:86884d5b174e2f64e900edd129b5ef0d2f784a65'
+c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$guSw+vJIguBLOY0B++LTLw$WQczkgbfW6YYN+aiO/3GMuQEToe7/XXbBb57fO9ugCU'
 c.NotebookApp.port = int(os.environ.get("PORT", 8888))
 c.NotebookApp.allow_root = True
-#c.NotebookApp.allow_password_change = True
+c.NotebookApp.allow_password_change = True
 c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
 
 # Configuration file for jupyter-notebook.
